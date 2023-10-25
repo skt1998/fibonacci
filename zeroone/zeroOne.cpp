f3bf6d2c658dbce255a0b03a3d65f1d81ba0939f
SURAJ KUMAR <sktivari1@gmail.com>
2023-10-25 22:32:55 +0530

    zeroone

---
+++ zeroone/zeroOne.cpp
@@ -0,0 +1,34 @@
+// zero one pattern
+//  1
+//  01
+//  101
+//  0101
+//  10101  
+// 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int n;
+   cin>>n;
+   for (int i = 1; i < n; i++)
+   {
+
+      for (int j = 1; j <= i; j++)
+      {
+
+         if ((i + j) % 2 == 0)
+         {
+            cout << "0";
+         }
+         else
+         {
+            cout << "1";
+         }
+      }
+      cout << endl;
+   }
+}
